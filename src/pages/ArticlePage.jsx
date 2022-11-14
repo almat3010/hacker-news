6a1e149783e9be8b8a809c45be1aecc3b73dabc3
almat3010 <almat.sultanov@gmail.com>
2022-11-14 03:20:35 +0300

    add ArticlePage

---
+++ src/pages/ArticlePage.jsx
@@ -0,0 +1,12 @@
+import CommentsList from '../components/CommentsList/CommentsList'
+import Title from '../components/Title/Title'
+
+const ArticlePage = () => {
+    return(
+        <>
+            <Title></Title>
+            <CommentsList/>
+        </>
+    )
+}
+export default ArticlePage
